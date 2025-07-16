4ce71457200e8788e5715d535e58b6a436197ee9
JeandeDieu Nkengfack <jeandedieu405@gmail.com>
2025-07-16 20:07:41 +0000

    Make the following changes:
    On the page src/app/transport/search/page.t

--- src/app/transport/search/ride-option-card.tsx
+++ src/app/transport/search/ride-option-card.tsx
@@ -5,13 +5,26 @@ import { Card, CardContent } from '@/components/ui/card';
 import { Badge } from '@/components/ui/badge';
 import { cn } from '@/lib/utils';
 import Image from 'next/image';
-import { Car, Bike, Shield, Users, User, Clock, ArrowLeft, Star, CarFront, PawPrint } from 'lucide-react';
+import { Shield, Users, User, Clock, ArrowLeft, Star, PawPrint } from 'lucide-react';
 
 export const MotorcycleIcon = (props: React.SVGProps<SVGSVGElement>) => (
-    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" {...props}>
-      <path d="M5 14h1"/><path d="M18 14h1"/><path d="m18 19-3-3 1-2h-5l1 2-3 3"/><path d="m14 14-2-3-2 3"/><path d="M8 10h8"/><path d="M5.5 10.5C4 11 3 13 3 14a2 2 0 0 0 2 2h.5"/><path d="M18.5 10.5c1.5.5 2.5 2.5 2.5 3.5a2 2 0 0 1-2 2h-.5"/><circle cx="6" cy="19" r="2"/><circle cx="18" cy="19" r="2"/>
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor" stroke="currentColor" strokeWidth="0" strokeLinecap="round" strokeLinejoin="round" {...props}>
+        <path d="M18.9,3.1c-0.6-0.3-1.3-0.1-1.7,0.4l-3.3,4.1H8.1l-3.3-4.1C4.4,2.9,3.7,2.8,3.1,3.1C2.5,3.4,2.3,4.1,2.6,4.7l3.7,4.7v5.8c0,0.5,0.4,1,1,1h1.5c0.5,0,1-0.4,1-1v-2.1h2.5v2.1c0,0.5,0.4,1,1,1H15c0.5,0,1-0.4,1-1v-5.8l3.7-4.7C20.2,4.1,19.5,3.4,18.9,3.1z M8.9,7.3h6.2L12,3.9L8.9,7.3z M7,21.8c-1.5,0-2.8-1.2-2.8-2.8s1.2-2.8,2.8-2.8s2.8,1.2,2.8,2.8S8.5,21.8,7,21.8z M17,21.8c-1.5,0-2.8-1.2-2.8-2.8s1.2-2.8,2.8-2.8s2.8,1.2,2.8,2.8S18.5,21.8,17,21.8z"/>
     </svg>
 );
+
+const Car = (props: React.SVGProps<SVGSVGElement>) => (
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor" stroke="currentColor" strokeWidth="0" strokeLinecap="round" strokeLinejoin="round" {...props}>
+        <path d="m21.7-2.4-1-3.6c-.2-.7-.8-1.1-1.5-1.1h-14c-.7 0-1.3.5-1.5 1.1l-1 3.6c-.1.2-.1.5 0 .7.1.3.4.5.7.5h1.3c.5 0 .9-.3 1.1-.7l.9-3.2h11l.9 3.2c.2.4.6.7 1.1.7h1.3c.3 0 .6-.2.7-.5.1-.2.1-.5 0-.7zM4.3 9.4l.6-2.1h14.2l.6 2.1H4.3zM2.8 20.3c-.6.2-1 .7-1 1.3v1.6c0 .5.4.9.9.9h.9c.4 0 .8-.4.9-.8l.4-1.6H5c.4 0 .7-.3.8-.7l.6-2.2h-3c-.4 0-.7.3-.8.7l-.3 1.1zM21.2 20.3c-.1-.4-.4-.7-.8-.7h-3l.6 2.2c.1.4.4.7.8.7h.6l.4 1.6c.1.4.5.8.9.8h.9c.5 0 .9-.4.9-.9v-1.6c0-.6-.4-1.1-1-1.3zM4.9 14.1h14.2c.5 0 .9-.4.9-.9V9.6c0-.5-.4-.9-.9-.9H4.9c-.5 0-.9.4-.9.9v3.6c0 .5.4.9.9.9z"/>
+    </svg>
+);
+
+const CarFront = (props: React.SVGProps<SVGSVGElement>) => (
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor" stroke="currentColor" strokeWidth="0" strokeLinecap="round" strokeLinejoin="round" {...props}>
+        <path d="M19,17h2c0.6,0,1-0.4,1-1v-3c0-0.9-0.7-1.7-1.5-1.9C18.7,10.6,16,10,16,10s-1.3-1.4-2.2-2.3c-0.5-0.4-1.1-0.7-1.8-0.7H5c-0.6,0-1.1,0.4-1.4,0.9l-1.4,2.9C2.1,11.5,2,11.7,2,12v4c0,0.6,0.4,1,1,1h2"/><path d="M12,10V7h1.8c0.4,0,0.9,0.1,1.2,0.3"/><path d="M5,10h1"/><circle cx="7" cy="17" r="2"/><path d="M9,17h6"/><circle cx="17" cy="17" r="2"/>
+    </svg>
+);
+
 
 export const rideOptions = [
     { 
