b83b2b2ab71950fe5b19b05a36047184eccf27a5
JeandeDieu Nkengfack <jeandedieu405@gmail.com>
2025-08-12 23:32:08 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

---
+++ src/next.config.js
@@ -0,0 +1,44 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+       {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'storage.googleapis.com',
+        port: '',
+        pathname: '/**',
+      }
+    ],
+  },
+  env: {
+    NEXT_PUBLIC_GOOGLE_MAPS_API_KEY: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      // Exclude server-only packages from client-side bundle
+      config.externals.push('long', 'protobufjs', '@grpc/grpc-js', 'nice-grpc-web');
+    }
+    return config;
+  },
+};
+
+module.exports = nextConfig;
