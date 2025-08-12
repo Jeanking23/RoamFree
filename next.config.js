1b1bb606cbe51eeaedaeebe4f88d56b2b0adf389
JeandeDieu Nkengfack <jeandedieu405@gmail.com>
2025-08-12 23:32:37 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.js
+++ next.config.js
@@ -21,6 +21,12 @@ const nextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'storage.googleapis.com',
+        port: '',
+        pathname: '/**',
+      }
     ],
   },
   env: {
