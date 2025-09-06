e4de1d58684e3ccfbe26d3f9f8cb843007d9decd
JeandeDieu Nkengfack <jeandedieu405@gmail.com>
2025-09-06 17:52:50 +0000

    Try fixing this error: `Unhandled Runtime Error: Error: React.Children.o

--- src/app/corporate-solutions-demo/page.tsx
+++ src/app/corporate-solutions-demo/page.tsx
@@ -112,7 +112,7 @@ export default function CorporateSolutionsDemoPage() {
                             />
                             <FormField control={form.control} name="message" render={({ field }) => (<FormItem><FormLabel>Your Message</FormLabel><FormControl><Textarea placeholder="Tell us about your company's travel needs..." {...field}/></FormControl><FormMessage/></FormItem>)}/>
                             <Button type="submit" size="lg" className="w-full bg-accent hover:bg-accent/90 text-accent-foreground">
-                                <Send className="mr-2 h-4 w-4"/> Submit Inquiry
+                                <span><Send className="mr-2 h-4 w-4"/> Submit Inquiry</span>
                             </Button>
                         </form>
                     </Form>
